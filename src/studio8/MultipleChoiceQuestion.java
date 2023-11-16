38bd24d0fbac8517d21c409a408532d3bb3393d5
rebec <rebec@RTYLaptop.dhcp.wustl.edu>
2023-11-16 12:45:17 -0600

    welp

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -1,38 +1,41 @@
 package studio8;
 
 public class MultipleChoiceQuestion extends Question {
-	
+	private String[] choices;
+
 	public MultipleChoiceQuestion(String prompt, String answer, int points, String[] choices) {
-		//FIXME
-		
-		//Call the super class constructor, then create and set
-		//instance variables for any values that aren't handled
-		//by the base class
+		super(prompt, answer, points);
+		// FIXME
+		this.choices = choices;
+		// Call the super class constructor, then create and set
+		// instance variables for any values that aren't handled
+		// by the base class
 	}
-	
+
 	public void displayPrompt() {
-		//FIXME
-		
+		// FIXME
+		System.out.println(this.getPrompt() + "(" + this.getPoints() + " points)");
+		int cChoice = 1;
+		for (int i = 0; i < choices.length; i++) {
+			System.out.println("choice" + cChoice + ":" + choices[i] + " ,");
+			cChoice++;
+		}
 		/*
-		 * Use the method from the base class to display the prompt,
-		 * Then write more code to display all of the choices like so:
+		 * Use the method from the base class to display the prompt, Then write more
+		 * code to display all of the choices like so:
 		 * 
-		 * 1. Choice1
-		 * 2. Choice2
-		 * 3. Choice3
-		 * ...
-		 * N. ChoiceN
+		 * 1. Choice1 2. Choice2 3. Choice3 ... N. ChoiceN
 		 */
 		//
 	}
-	
+
 	public static void main(String[] args) {
-		String[] choices = {"seven", "nine", "eight", "six"};
-		Question multipleChoice = new MultipleChoiceQuestion("What studio is this?", "3", 1, choices);
+		String[] choices = { "seven", "nine", "eight", "six" };
+		Question multipleChoice = new MultipleChoiceQuestion("What number am I thinking of?", "nine", 1, choices);
 		multipleChoice.displayPrompt();
-		System.out.println(multipleChoice.checkAnswer("hi"));//wrong
-		System.out.println(multipleChoice.checkAnswer("1"));//wrong
-		System.out.println(multipleChoice.checkAnswer("3"));//right
+		System.out.println(multipleChoice.checkAnswer("hi"));// wrong
+		System.out.println(multipleChoice.checkAnswer("six"));// wrong
+		System.out.println(multipleChoice.checkAnswer("nine"));// right
 	}
 
 }
